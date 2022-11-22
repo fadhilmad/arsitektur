b2145a350ae44658755e51b2fd77726e5a1dee26
Fadil <ffaza46@gmail.com>
2022-11-22 23:33:34 +0700

    membagi tampilan views di langding page dan tambah tampilan view about

---
+++ resources/views/layout/landing_page/header.blade.php
@@ -0,0 +1,37 @@
+<nav class="site-nav">
+    <div class="container">
+        <div class="menu-bg-wrap">
+            <div class="site-navigation">
+                <a href="index.html" class="logo m-0 float-start">DSATELI3R</a>
+
+                <ul class="js-clone-nav d-none d-lg-inline-block text-start site-menu float-end">
+                    <li class="active"><a href="index.html">Home</a></li>
+                    <li class="has-children">
+                        <a href="properties.html">Projects</a>
+                        <ul class="dropdown">
+                            <li><a href="#">All Projects</a></li>
+                            <li><a href="#">Buy Projects</a></li>
+                            <li><a href="#">Sell Projects</a></li>
+                            
+                            {{-- <li class="has-children">
+                                <a href="#">Dropdown</a>
+                                <ul class="dropdown">
+                                    <li><a href="#">Sub Menu One</a></li>
+                                    <li><a href="#">Sub Menu Two</a></li>
+                                    <li><a href="#">Sub Menu Three</a></li>
+                                </ul>
+                            </li> --}}
+                        </ul>
+                    </li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="contact.html">Contact Us</a></li>
+                </ul>
+
+                <a href="#" class="burger light me-auto float-end mt-1 site-menu-toggle js-menu-toggle d-inline-block d-lg-none" data-toggle="collapse" data-target="#main-navbar">
+                    <span></span>
+                </a>
+
+            </div>
+        </div>
+    </div>
+</nav>
