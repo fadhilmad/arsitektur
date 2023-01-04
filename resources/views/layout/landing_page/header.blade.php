0a55f9f33709c3d707c7e6adcb8c8c83c405f61b
unknown <ffaza46@gmail.com>
2023-01-04 23:54:48 +0700

    memperbaiki yang kurang pas

--- resources/views/layout/landing_page/header.blade.php
+++ resources/views/layout/landing_page/header.blade.php
@@ -3,15 +3,15 @@
 
         <a><img href="/" src="{{url('assets/img/img-5-blt.png')}}" class="rounded-2" style="widht:10px; height:35px" alt=""></a>
 
-        <ul class="js-clone-nav d-none d-lg-inline-block text-start site-menu float-end">
+        <ul class="js-clone-nav d-none d-lg-inline-block text-start site-menu float-end" >
             <li class="active"><a href="/">Home</a></li>
             <li class="has-children">
                 <a href="#">Project</a>
-                <ul class="dropdown">
+                <ul class="dropdown rounded-3">
                     <li><a href="/interios">Interiors</a></li>
                     <li class="has-children">
                         <a href="/arsitekture">Architecture</a>
-                        <ul class="dropdown">
+                        <ul class="dropdown rounded-3">
                             <li><a href="/commercial_index">Commercial</a></li>
                             <li><a href="/residential_index">Residential</a></li>
                             <li><a href="/retail_index">Retail</a></li>
@@ -24,7 +24,7 @@
             <li><a href="/contact">Contact Us</a></li>
             <li><a href="#">Translate</a></li>
 
-
+            
             <li><a class="btn btn-success text-black" href="{{url ('/login')}}">login</a></li>
         </ul>
         <a href="#" class="burger light me-auto float-end mt-1 site-menu-toggle js-menu-toggle d-inline-block d-lg-none" data-toggle="collapse" data-target="#main-navbar">
